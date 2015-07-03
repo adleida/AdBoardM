77cd78dadb36b4bdb419fdabf636d971cd33d03f
lkiversonlk <city.of.beijing@gmail.com>
2015-07-03 12:54:39 +0800

    add puttings router

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var session = require("express-session");
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var puttings = require("./routes/puttings");
 var app = express();
 
 // view engine setup
@@ -38,6 +38,7 @@ passport.deserializeUser(function(user, done) {
 
 app.use('/', routes);
 app.use('/users', users);
+app.use("/puttings", puttings);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
